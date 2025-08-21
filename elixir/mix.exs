7ed82eca372204b643e6d004a272ef87e15d2603
yurylyt <yury@yury-lytvynenko.com>
2025-08-21 22:12:43 +0300

    downgrade elixir version, script for growing community

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -5,7 +5,7 @@ defmodule MiniSim.MixProject do
     [
       app: :mini_sim,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
---
+++ growing_community.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+MIN_COMM_SIZE=2
+MAX_COMM_SIZE=10
+
+ITERS=10
+SEED=42
+CHUNK_SIZE=256
+
+run_simulation() {
+    local language=$1
+    echo "$language"
+    for i in $(seq "$MIN_COMM_SIZE" "$MAX_COMM_SIZE"); do
+        ./run_sim.sh "$language" --agents "$i" --iterations "$ITERS" --seed "$SEED" --chunk-size "$CHUNK_SIZE"
+    done
+}
+
+echo "Community size [$MIN_COMM_SIZE..$MAX_COMM_SIZE]"
+run_simulation "python"
+echo
+run_simulation "elixir"
