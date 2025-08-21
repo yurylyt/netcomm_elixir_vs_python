4deaa45d1fb503a768bbc69085c82deb445e5f12
yurylyt <yury@yury-lytvynenko.com>
2025-08-21 23:05:10 +0300

    num proc as a parameter

--- growing_community.sh
+++ growing_community.sh
@@ -6,12 +6,13 @@ MAX_COMM_SIZE=10
 ITERS=10
 SEED=42
 CHUNK_SIZE=256
+NUM_PROCS=${1:-1}
 
 run_simulation() {
     local language=$1
     echo "$language"
     for i in $(seq "$MIN_COMM_SIZE" "$MAX_COMM_SIZE"); do
-        ./run_sim.sh "$language" --agents "$i" --iterations "$ITERS" --seed "$SEED" --chunk-size "$CHUNK_SIZE"
+        ./run_sim.sh "$language" --agents "$i" --iterations "$ITERS" --seed "$SEED" --chunk-size "$CHUNK_SIZE" --procs "$NUM_PROCS"
     done
 }
 
